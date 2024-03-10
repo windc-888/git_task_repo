ccf50ac28356e219752bfb3849197587c290050c
windc-888 <windhy.cahyadi@gmail.com>
2024-03-10 17:13:56 +0000

    Added comments to git_task.py

--- git_task.py
+++ git_task.py
@@ -1,3 +1,8 @@
+# This script asks for the user to input a sentence and will return the 
+# sentence but with all caps.
+
+# Asking for user input.
 sent = input("Please input a sentence:")
 
-print(sent.capitalize())
+# Printing the capitalised sentence using .upper().
+print(f"This is your sentence in all caps: {sent.upper()}")
